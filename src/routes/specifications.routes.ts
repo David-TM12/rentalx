0758a739187910af41f90204af2ceeeaa3d10a64
DavidTM <deivi_gf144@hotmail.com>
2021-10-03 11:26:32 -0400

    Criando token de usuario, middleware de authenticate

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { CreateSpecificationController } from "../module/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../module/cars/useCases/listSpecifications/ListSpecificationsController";
-
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const specificationsRoutes = Router();
 
 
 const createSpecificationUseCase = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationUseCase.handle);
 
 specificationsRoutes.get("/", listSpecificationsController.handle);
