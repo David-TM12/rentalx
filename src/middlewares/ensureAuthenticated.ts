d840a14c3fdbf6a2e9d9f493c7136972bc284eae
DavidTM <deivi_gf144@hotmail.com>
2021-10-03 11:51:10 -0400

    Criando tratamento de excessoes AppError, lib async-express-errors

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import {Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../module/accounts/repositories/implementations/UsersRepository";
 
 interface IPayload {
@@ -8,33 +9,30 @@ interface IPayload {
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
  
-    const authHeader = request.headers.authorization;
+  const authHeader = request.headers.authorization;
 
-    if(!authHeader){
-        throw new Error("Token missing!");     
-    }
+  if(!authHeader){
+    throw new AppError("Token missing!", 401);     
+  }
 
-    const [, token] = authHeader.split(" ");
+  const [, token] = authHeader.split(" ");
 
-    try{
-        const { sub: user_id } = verify(token, "2971df05420ee7705ee22006a4dacbb2") as IPayload;
+  try{
+    const { sub: user_id } = verify(token, "2971df05420ee7705ee22006a4dacbb2") as IPayload;
 
-        const usersRepository = new UsersRepository();
+    const usersRepository = new UsersRepository();
 
-       const user =  usersRepository.findById(user_id);
+    const user =  usersRepository.findById(user_id);
 
-       if(!user){
-        throw new Error("User does not exists!!!");
-        
-       }
-        next();
-    }catch{
-        throw new Error("Invalid Ttoken!");
-        
+    if(!user){
+    throw new AppError("User does not exists!!!", 401);
     }
     
-
-
-
+    next();
+  }catch{
+    throw new AppError("Invalid Ttoken!", 401);
+      
+  }
+  
 
 }
